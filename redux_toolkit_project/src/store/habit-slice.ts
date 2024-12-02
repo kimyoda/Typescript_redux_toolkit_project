b48f856b7fea417ae5e00def93f672773db775a8
kimyoda <rladygks1210@naver.com>
2024-12-03 01:18:34 +0900

    [feat] addHabit action add

--- redux_toolkit_project/src/store/habit-slice.ts
+++ redux_toolkit_project/src/store/habit-slice.ts
@@ -1,9 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Habit {
   id: string;
   name: string;
-  frequency: "daliy" | "weekly";
+  frequency: "daily" | "weekly";
   completedDates: string[];
   createdAt: string;
 }
@@ -20,7 +20,20 @@ const habitSlice = createSlice({
   name: "habits",
   initialState,
   reducers: {
-    addHabit: () => {},
+    addHabit: (
+      state,
+      action: PayloadAction<{ name: string; frequency: "daily" | "weekly" }>
+    ) => {
+      const newHabit: Habit = {
+        id: Date.now().toString(),
+        name: action.payload.name,
+        frequency: action.payload.frequency,
+        completedDates: [],
+        createdAt: new Date().toISOString(),
+      };
+
+      state.habits.push(newHabit);
+    },
   },
 });
 
