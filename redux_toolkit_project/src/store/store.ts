a3e8f62989afd73b7ed276c9490648cef8f62558
kimyoda <rladygks1210@naver.com>
2024-12-03 00:18:46 +0900

    [feat] dispatch, habits add

--- redux_toolkit_project/src/store/store.ts
+++ redux_toolkit_project/src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import habitsReducer from "./habit-slice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     habits: habitsReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AddDispatch = typeof store.dispatch;
+
+export default store;
